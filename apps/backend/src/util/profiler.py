1f61c431a318b9ee3fcc3af913675a44a4b3e24b
skysw <skwa00001@stud.uni-saarland.de>
2026-06-11 20:54:26 +0200

    add profiler

---
+++ apps/backend/src/util/profiler.py
@@ -0,0 +1,20 @@
+import time
+from contextlib import contextmanager
+
+from loguru import logger
+
+
+@contextmanager
+def profile(name: str):
+    """
+    Profiles a code block,
+
+    Example:
+    with profile("example"):
+        sleep(1)
+
+    ==> prints: [PROFILED] example: 1s
+    """
+    start = time.perf_counter()
+    yield
+    logger.debug(f"[PROFILE] {name}: {time.perf_counter() - start:.4f}s")
