c8c8cbc95708965df4d4b97752866f25561f809e
Linlin Li <lilinlin0502@outlook.com>
2026-06-17 16:26:57 +0200

    revert project request

--- packages/api/src/projects.ts
+++ packages/api/src/projects.ts
@@ -20,6 +20,7 @@ export interface ProjectResponse {
 export interface CreateProjectRequest {
   label: string;
   agent_id: number;
+  video_ids: number[];
   knowledge_id: number | null;
   idle_video_id: number | null;
   enter_video_id: number | null;
