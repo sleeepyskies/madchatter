4a2c76b3ad26114ad191b7f4715c340869c98088
skysw <skwa00001@stud.uni-saarland.de>
2026-06-19 08:48:59 +0200

    better handling for api client

--- packages/api/src/client.ts
+++ packages/api/src/client.ts
@@ -1,11 +1,20 @@
 import ky from "ky";
 
-const baseUrl = new URL(
-    `http://${process.env.ADDRESS ?? "127.0.0.1"}:${process.env.SERVER_PORT ?? "8000"}/api`
-).toString();
+const address = process.env.ADDRESS;
+const port = process.env.PORT;
+
+if (!address) {
+  throw new Error("Can not init api, server address is undefined");
+}
+
+if (!port) {
+  throw new Error("Can not init api, server port is undefined");
+}
+
+const baseUrl = new URL(`http://${address}:${port}/api`).toString();
 
 export const client = ky.create({
-    prefix: baseUrl,
-    timeout: 10_000,
-    retry: 1,
+  prefix: baseUrl,
+  timeout: 10_000,
+  retry: 1,
 });
