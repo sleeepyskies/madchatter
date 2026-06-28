e5278a96b8a5bfa23f764b7bfa40b9abdd950b2a
sky <skyswalla@gmail.com>
2026-06-28 20:52:16 +0200

    Remove theme files, fixes frontend build

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -4,9 +4,8 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import ThemeProvider from "@/providers/theme-provider";
 
-const notoSans = Noto_Sans({subsets: ["latin"], variable: "--font-sans"});
+const notoSans = Noto_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +23,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
@@ -40,14 +39,12 @@ export default function RootLayout({
         notoSans.variable,
       )}
     >
-    <body className="min-h-full flex flex-col">
-    <ThemeProvider>
-      <TooltipProvider delayDuration={0}>
-        {children}
-        <Toaster/>
-      </TooltipProvider>
-    </ThemeProvider>
-    </body>
+      <body className="min-h-full flex flex-col">
+        <TooltipProvider delayDuration={0}>
+          {children}
+          <Toaster />
+        </TooltipProvider>
+      </body>
     </html>
   );
 }
